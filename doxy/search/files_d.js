1b38e2fae749b6509181e768c05fe0ee18d4a9e3
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2015-10-20 12:16:44 -0400

    add to_group

--- doxy/search/files_d.js
+++ doxy/search/files_d.js
@@ -5,6 +5,7 @@ var searchData=
   ['set_5fintersect_2eh',['set_intersect.h',['../d0/dc2/set__intersect_8h.html',1,'']]],
   ['set_5fsym_5fdiff_2eh',['set_sym_diff.h',['../de/d87/set__sym__diff_8h.html',1,'']]],
   ['set_5funion_2eh',['set_union.h',['../dc/d72/set__union_8h.html',1,'']]],
+  ['shuffle_2eh',['shuffle.h',['../d3/d2f/shuffle_8h.html',1,'']]],
   ['skip_2eh',['skip.h',['../d4/d8e/skip_8h.html',1,'']]],
   ['slice_2eh',['slice.h',['../d0/d48/slice_8h.html',1,'']]],
   ['sort_2eh',['sort.h',['../d7/d50/sort_8h.html',1,'']]],
