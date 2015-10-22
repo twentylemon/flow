5d3f3001bdf93426e374d47561aad455435a3a3b
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2015-10-22 13:06:23 -0400

    add inner_product

--- doxy/search/files_6.js
+++ doxy/search/files_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['inner_5fproduct_2eh',['inner_product.h',['../d4/d75/inner__product_8h.html',1,'']]],
   ['iota_2eh',['iota.h',['../db/d3e/iota_8h.html',1,'']]],
   ['iterate_2eh',['iterate.h',['../d0/dbc/iterate_8h.html',1,'']]]
 ];
