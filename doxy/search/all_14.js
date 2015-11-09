5078f75562d7a89160f23e53dfc04a87a3e64541
Taras Mychaskiw <taras.mychaskiw@gmail.com>
2015-11-09 11:10:17 -0500

    typos

---
+++ doxy/search/all_14.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['_7eoptional',['~optional',['../d6/d3f/classflow_1_1optional.html#a46448d190e234179fefb40612eb92803',1,'flow::optional']]]
+];
--- doxy/search/files_a.js
+++ doxy/search/files_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['optional_2eh',['optional.h',['../dc/d62/optional_8h.html',1,'']]]
+  ['optional_2eh',['optional.h',['../dc/d62/optional_8h.html',1,'']]],
+  ['optional2_2eh',['optional2.h',['../d4/d85/optional2_8h.html',1,'']]]
 ];
